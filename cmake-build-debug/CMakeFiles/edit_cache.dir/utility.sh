202f9a3f34865b18403de8a224bcace62d5276c2
Harry-Dryden <harrydryden27@gmail.com>
2025-05-21 14:48:31 +0100

    adding placeholders

--- cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -1,4 +1,4 @@
 set -e
 
-cd /cygdrive/c/Users/Bland/CLionProjects/CGame/cmake-build-debug/CMakeFiles/CMakeScratch/TryCompile-fs3KE5
+cd /cygdrive/c/Users/Bland/CLionProjects/CGame/cmake-build-debug
 /cygdrive/c/Users/Bland/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
--- cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
+++ cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -1,4 +1,4 @@
 set -e
 
-cd /cygdrive/c/Users/Bland/CLionProjects/CGame/cmake-build-debug/CMakeFiles/CMakeScratch/TryCompile-fs3KE5
+cd /cygdrive/c/Users/Bland/CLionProjects/CGame/cmake-build-debug
 /cygdrive/c/Users/Bland/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
